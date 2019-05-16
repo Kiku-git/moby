tiborvass/docker

Add more import comments
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

pkg/capabilities/caps.go
@@ -1,5 +1,5 @@
 // Package capabilities allows to generically handle capabilities.
-package capabilities
+package capabilities // import "github.com/tiborvass/docker/pkg/capabilities"
 
 // Set represents a set of capabilities.
 type Set map[string]struct{}

pkg/capabilities/caps_test.go
@@ -1,4 +1,4 @@
-package capabilities
+package capabilities // import "github.com/tiborvass/docker/pkg/capabilities"
 
 import (
 	"fmt"

rootless/rootless.go
@@ -1,4 +1,4 @@
-package rootless
+package rootless // import "github.com/tiborvass/docker/rootless"
 
 import (
 	"os"

rootless/specconv/specconv_linux.go
@@ -1,4 +1,4 @@
-package specconv
+package specconv // import "github.com/tiborvass/docker/rootless/specconv"
 
 import (
 	"io/ioutil"

Kiku-git/moby

pkg/capabilities/caps.go
@@ -1,5 +1,5 @@
 // Package capabilities allows to generically handle capabilities.
-package capabilities
+package capabilities // import "github.com/docker/docker/pkg/capabilities"
 
 // Set represents a set of capabilities.
 type Set map[string]struct{}

pkg/capabilities/caps_test.go
@@ -1,4 +1,4 @@
-package capabilities
+package capabilities // import "github.com/docker/docker/pkg/capabilities"
 
 import (
 	"fmt"

rootless/rootless.go
@@ -1,4 +1,4 @@
-package rootless
+package rootless // import "github.com/docker/docker/rootless"
 
 import (
 	"os"

rootless/specconv/specconv_linux.go
@@ -1,4 +1,4 @@
-package specconv
+package specconv // import "github.com/docker/docker/rootless/specconv"
 
 import (
 	"io/ioutil"